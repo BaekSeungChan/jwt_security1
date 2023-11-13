eca05f9fc1932670b357843c1eaf1bc194cc5504
baekseungchan1117 <bsc7386@gmail.com>
2023-11-13 21:47:02 +0900

    first commit

---
+++ src/test/java/com/qortmdcks/jwt_security1/JwtSecurity1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qortmdcks.jwt_security1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSecurity1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
